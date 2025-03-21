0a283d20ff7cff84bdea17e6cb23061f0cdcbbc6
Sam <58605163+saba-tandashvili@users.noreply.github.com>
2025-03-21 23:06:19 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const audioz = document.querySelector(".audio")
 body.addEventListener('click', () => {
     audioz.play();
 })
-body.addEventListener('mouseover', () => {
-    audioz.play();
-})
+
+window.onload = function(){
+const audioz = document.querySelector(".audio").play()
+}
